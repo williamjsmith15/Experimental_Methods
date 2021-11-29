781d9b18bb2e1fb8fbdb1a7d338efa6f85ffec8e
William Smith <williamjsmith15@hotmail.co.uk>
2021-11-29 14:25:54 +0000

    Add time to X-Axis using input for sample frequency

--- EM_Analysis.m
+++ EM_Analysis.m
@@ -4,6 +4,16 @@ fprintf("Select the raw data file to be analysed.\n\n")
 [fname_data, path_data] = uigetfile('*.csv');
 data = readtable(fullfile(path_data, fname_data)); %? Also use csvread if this doesnt work...
 
+%Sample freq, time period etc
+sample_freq = input("Enter the sampling freq of the data (Hz).");
+T = 1/sample_freq;
+L = length(data(:,1)); %Length of data
+t = (0:L-1) * T; %Time vector (s)
+
+
+
+
+
 %------APPLY CALIBRATION------
 %Import the calibration file
 fprintf("Select the calibration data file to be analysed.\n\n")
@@ -14,20 +24,36 @@ calibration_matrix = readtable(fullfile(path_calibration, fname_calibration));
 data(:,4) = 1;
 calibrated_data = data * calibration_matrix;
 
+%Normalise the Z Axis to 0 (As only looking at the acceleration extra to g)
+calibrated_data(:,3) = calibrated_data(:,3) - 1;
+
 %Plot the calibrated data and save plot to folder of fname_data
-f1 = figure;
-figure(f1);
+fig_raw = figure;
+figure(fig_raw);
 hold on;
-plot(calibrated_data(:,1));
-plot(calibrated_data(:,2));
-plot(calibrated_data(:,3));
-xlabel("Data Point");
+plot(t, calibrated_data(:,1));
+plot(t, calibrated_data(:,2));
+plot(t, calibrated_data(:,3));
+xlabel("Time (s)");
 ylabel("Acceleration (g)");
 title("Raw Calibrated Data Plot");
 
 
+
+
+
+
 %------SPECTRAL ANALYSIS OF DATA------
-%Need FFT, peak finder, ploting
+%Need FFT, peak finder, ploting - which way do we want to do this, add all
+%the values together, average XYZ, just take XYZ all seperatley??
+
+%FFT of just z for now, possible to do for other axes?
+freq_data = fft(calibrated_data(:,3));
+
+%Plot the fft
+fig_fft = figure;
+figure(fig_fft);
+plot(freq_data);
 
 
 

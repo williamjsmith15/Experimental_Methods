0548581776655ce43d464cadf1c9c5ce4d69eae7
William Smith <williamjsmith15@hotmail.co.uk>
2021-11-29 14:10:30 +0000

    Fix issue with importing file, Add plot of raw data

--- EM_Analysis.m
+++ EM_Analysis.m
@@ -1,18 +1,29 @@
 
 %------INPUT DATA------
 fprintf("Select the raw data file to be analysed.\n\n")
-fname_data = uigetfile('*.csv');
-data = readtable(fname_data); %? Also use csvread if this doesnt work...
+[fname_data, path_data] = uigetfile('*.csv');
+data = readtable(fullfile(path_data, fname_data)); %? Also use csvread if this doesnt work...
 
 %------APPLY CALIBRATION------
+%Import the calibration file
 fprintf("Select the calibration data file to be analysed.\n\n")
-fname_calibration = uigetfile('*.csv');
-calibration_matrix = readtable(fname_calibration);
+[fname_calibration, path_calibration] = uigetfile('*.csv');
+calibration_matrix = readtable(fullfile(path_calibration, fname_calibration));
 
+%Apply the calibration
 data(:,4) = 1;
 calibrated_data = data * calibration_matrix;
 
-
+%Plot the calibrated data and save plot to folder of fname_data
+f1 = figure;
+figure(f1);
+hold on;
+plot(calibrated_data(:,1));
+plot(calibrated_data(:,2));
+plot(calibrated_data(:,3));
+xlabel("Data Point");
+ylabel("Acceleration (g)");
+title("Raw Calibrated Data Plot");
 
 
 %------SPECTRAL ANALYSIS OF DATA------

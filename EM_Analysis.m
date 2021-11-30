cc7e5f9d25d0dc9690fab8af91c44c47e7999fa0
William Smith <williamjsmith15@hotmail.co.uk>
2021-11-30 13:34:32 +0000

    Fix table length error and plot raw calibrated data before removing 1g in z-axis

--- EM_Analysis.m
+++ EM_Analysis.m
@@ -3,7 +3,7 @@
 fprintf("Select the raw data file to be analysed.\n")
 [fname_data, path_data] = uigetfile('*.csv');
 fprintf("Opening %s\n\n", fname_data)
-data = readtable(fullfile(path_data, fname_data)); %? Also use csvread if this doesnt work...
+data = table2array(readtable(fullfile(path_data, fname_data)));
 
 %Sample freq, time period etc
 sample_freq = input("Enter the sampling freq of the data (Hz): ");
@@ -20,14 +20,11 @@ t = (0:L-1) * T; %Time vector (s)
 fprintf("Select the calibration data file to be analysed.\n")
 [fname_calibration, path_calibration] = uigetfile('*.csv');
 fprintf("Opening %s\n\n", fname_calibration)
-calibration_matrix = readtable(fullfile(path_calibration, fname_calibration));
+calibration_matrix = table2array(readtable(fullfile(path_calibration, fname_calibration)));
 
 %Apply the calibration
 data(:,4) = 1;
 calibrated_data = data * calibration_matrix;
-
-%Normalise the Z Axis to 0 (As only looking at the acceleration extra to g)
-calibrated_data(:,3) = calibrated_data(:,3) - 1;
 
 %Plot the calibrated data and save plot to folder of fname_data
 fig_raw = figure;
@@ -39,6 +36,11 @@ plot(t, calibrated_data(:,3));
 xlabel("Time (s)");
 ylabel("Acceleration (g)");
 title("Raw Calibrated Data Plot");
+
+%Normalise the Z Axis to 0 (As only looking at the acceleration extra to g)
+calibrated_data(:,3) = calibrated_data(:,3) - 1;
+
+
 
 
 
